579128b4f838d74cc1353e629b7f96a2b379e606
lflg04 <1584195298@qq.com>
2026-06-14 22:34:28 +0800

    Update site title to Lflg_Blog

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: '- Lflg\'s blog ',
+  title: 'Lflg_Blog',
   subtitle: '',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
